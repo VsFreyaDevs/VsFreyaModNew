788427eacfa074a495f64a61e5d8382a6cf4819b
CharlesCatYT <sptubea@gmail.com>
2024-10-16 18:54:20 -0400

    unexpected fucking slash bro

--- source/Main.hx
+++ source/Main.hx
@@ -47,7 +47,8 @@ class Main extends Sprite
     #else
     var framerate:Int = 144; // How many frames per second the game should run at.
     #end
-   */ / var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
+   */
+  var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
   var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
   // You can pretty much ignore everything from here on - your code should go in your states.
