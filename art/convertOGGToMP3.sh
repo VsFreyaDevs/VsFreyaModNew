e6a18c89e28ed914e54bac18b49839e69b253a4b
CharlesCatYT <sptubea@gmail.com>
2024-10-25 21:40:12 -0400

    Update convertOGGToMP3.sh

--- art/convertOGGToMP3.sh
+++ art/convertOGGToMP3.sh
@@ -2,4 +2,4 @@
 
 # Find all files in OGG format and execute ffmpeg to convert them to MP3
 # Skips files that are already in MP3 format
-find "../assets/" -name "*.ogg" -exec sh -c 'ffmpeg -n -i {} "${0%.*}.mp3"' {} \;
+find "../assets/" -name "*.ogg" -exec sh -c './ffmpeg -n -i {} "${0%.*}.mp3"' {} \;
