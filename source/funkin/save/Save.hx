171ce4a8c77d18347d29bbf074952b56d8b0d7c1
CharlesCatYT <sptubea@gmail.com>
2024-10-31 19:46:53 -0400

    Update Save.hx

--- source/funkin/save/Save.hx
+++ source/funkin/save/Save.hx
@@ -21,10 +21,8 @@ class Save
   public static final SAVE_DATA_VERSION_RULE:thx.semver.VersionRule = ">=2.1.0 <2.2.0";
 
   // We load this version's saves from a new save path, to maintain SOME level of backwards compatibility.
-  // static final SAVE_PATH:String = 'VsFreyaDevs';
-  // static final SAVE_NAME:String = 'VsFreyaFennec';
-  static final SAVE_PATH:String = 'FunkinCrew';
-  static final SAVE_NAME:String = 'Funkin';
+  static final SAVE_PATH:String = 'VsFreyaDevs';
+  static final SAVE_NAME:String = 'VsFreyaFennec';
 
   static final SAVE_PATH_LEGACY:String = 'ninjamuffin99';
   static final SAVE_NAME_LEGACY:String = 'funkin';
@@ -968,7 +966,9 @@ class Save
 
     try
     {
-      trace(SAVE_NAME + " - SLOT: " + slot + " - PATH: " + SAVE_PATH);
+      if (FlxG.save.isEmpty()) trace(SAVE_NAME + " - SLOT: " + slot + " - idk what save file i actually saved so uhhh imma make a new one rq");
+      else
+        trace(SAVE_NAME + " - SLOT: " + slot + " - PATH: " + SAVE_PATH);
     }
     catch (e)
     {
