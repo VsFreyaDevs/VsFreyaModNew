5d7859e6550d659fc2d4d14143ecf085da6467cb
CharlesCatYT <sptubea@gmail.com>
2024-11-05 00:11:46 -0500

    bruh

--- source/funkin/modding/base/ScriptedFlxCamera.hx
+++
@@ -1,8 +0,0 @@
-package funkin.modding.base;
-
-/**
- * A script that can be tied to an FlxCamera.
- * Create a scripted class that extends FlxCamera to use this.
- */
-@:hscriptClass
-class ScriptedFlxCamera extends flixel.FlxCamera implements HScriptedClass {}
--- source/funkin/modding/base/ScriptedFlxShader.hx
+++
@@ -1,8 +0,0 @@
-package funkin.modding.base;
-
-/**
- * A script that can be tied to an FlxShader.
- * Create a scripted class that extends FlxShader to use this.
- */
-@:hscriptClass
-class ScriptedFlxShader extends flixel.system.FlxAssets.FlxShader implements HScriptedClass {}
